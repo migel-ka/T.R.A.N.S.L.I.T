056633428632b1c04417829e0b3337fe3beb5729
Mikhail Sidorov <mu_sidorov@mail.ru>
2024-11-23 12:37:42 +0300

    исправил нумерацию

--- src/components/dictionary/index.tsx
+++ src/components/dictionary/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC } from "react";
 import style from "./style.module.css";
 import cyrillicToTranslit from "cyrillic-to-translit-js";
 import DEL from "..//img/Del.png";
@@ -12,64 +12,34 @@ const Dictionary: FC<Props> = ({ words, onDelete }) => {
   return (
     <ol className={style.container}>
       <li className={style.containerLi}>
-        <div className={style.containerLiGrey}>Привет 👋🏻</div>
-        <div className={style.containerLiDarkGrey}>Privet</div>
+        <div className={style.containerLiGrey}>1. Привет 👋🏻</div>
+        <div className={style.containerLiDarkGrey}>
+          Privet{" "}
+          <button className={style.containerLiBtn}>
+            <img src={DEL} alt="close" />
+          </button>
+        </div>
       </li>
       {words.map((word, index) => (
-        <WordItem key={index} word={word} index={index} onDelete={onDelete} />
+        <li key={index} className={style.containerLi}>
+          <div className={style.containerLiGrey}>
+            {`${index + 2}. `}
+            <div className={style.containerLiGreyText}>{word}</div>
+          </div>
+          <div className={style.containerLiDarkGrey}>
+            <div className={style.containerLiDarkGreyText}>
+              {cyrillicToTranslit().transform(word)}
+            </div>
+            <button
+              className={style.containerLiBtn}
+              onClick={() => onDelete(index)}
+            >
+              <img src={DEL} alt="close" />
+            </button>
+          </div>
+        </li>
       ))}
     </ol>
-  );
-};
-
-const WordItem: FC<{
-  word: string;
-  index: number;
-  onDelete: (index: number) => void;
-}> = ({ word, index, onDelete }) => {
-  const [showTooltip, setShowTooltip] = useState(false);
-  const wordRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const checkOverflow = () => {
-      if (wordRef.current) {
-        const isOverflowing =
-          wordRef.current.scrollWidth > wordRef.current.clientWidth;
-        setShowTooltip(isOverflowing);
-      }
-    };
-
-    checkOverflow();
-    window.addEventListener("resize", checkOverflow);
-
-    return () => {
-      window.removeEventListener("resize", checkOverflow);
-    };
-  }, [word]);
-
-  return (
-    <li className={style.containerLi}>
-      <div className={style.containerLiGrey} ref={wordRef}>
-        {showTooltip && <div className={style.tollTip}>{word}</div>}
-        {word}
-      </div>
-      <div className={style.containerLiDarkGrey}>
-        {showTooltip && (
-          <div className={style.tollTip}>
-            {cyrillicToTranslit().transform(word)}
-          </div>
-        )}
-        <div className={style.containerLiDarkGreyText}>
-          {cyrillicToTranslit().transform(word)}
-        </div>
-        <button
-          className={style.containerLiBtn}
-          onClick={() => onDelete(index)}
-        >
-          <img src={DEL} alt="close" />
-        </button>
-      </div>
-    </li>
   );
 };
 
